de69c98c689b42e27d9dfb33a3effc6700842d20
ArtofBugs <TheArtofBugs@gmail.com>
2021-08-11 10:48:46 -0700

    A few additional fixes/clean-ups for Board.java

--- Board.java
+++ Board.java
@@ -7,14 +7,15 @@ import java.util.ArrayList;
 public class Board {
     int rows, cols;
     boolean squares[][];
+    // See NonParser for explanation of clue layouts.
     ArrayList<int[]> rowClues = new ArrayList<int[]>();
     ArrayList<int[]> colClues = new ArrayList<int[]>();
-    //as in, filled and unfilled -- colored nonograms not yet supported
-    final boolean BLACK = true;
-    final boolean WHITE = false;
-    //an additional idea (for whenever, if ever, colored nonograms will
-    //be supported): instead of true vs false, colors could be indicated by ints
-    //see NonParser for explanation of clue layouts
+    // True/false currently refer to filled and unfilled;
+    // colored nonograms not yet supported. If or when they
+    // become supported, colors could be indicated by ints.
+    final static boolean BLACK = true;
+    final static boolean WHITE = false;
+   
 
     public Board(int rows, int cols) {
         this.rows = rows;
@@ -22,12 +23,14 @@ public class Board {
         this.squares = new boolean[rows][cols];
     }
     
-    //Creates a new Board that is a copy of this one
+    // Returns a new Board that is a copy of this one.
     public Board clone() {
         Board result = new Board(this.rows, this.cols);
-        for (int r = 0; r < this.rows; r++)
-            for (int c = 0; c < this.cols; c++)
+        for (int r = 0; r < this.rows; r++) {
+            for (int c = 0; c < this.cols; c++) {
                 result.set(r, c, this.get(r, c));
+			}
+		}
         return result;
     }
 
@@ -47,11 +50,11 @@ public class Board {
     	return cols;
     }
     
-    public static int[] generateClues(bool[] line) {
+    public static int[] generateClues(boolean[] line) {
         ArrayList<Integer> currClues = new ArrayList<Integer>();
         // Count of how many filled squares in succession we've seen.
         int currCount = 0; 
-        for (s: bool in line) {
+        for (boolean s: line) {
             if (s == BLACK) {
                 currCount++;
             }
@@ -61,61 +64,56 @@ public class Board {
                 }
                 currCount = 0;
             }
-            if (currCount != 0) {
-                currClues.add(currCount);
-            }
-            int[] result = new int[currClues.size()];
-            for (int i = 0; i < currClues.size(); i++) {
-                result[i] = currClues.get(i);
-            }
-            return result;
-	}
+        }
+        if (currCount != 0) {
+            currClues.add(currCount);
+        }
+        int[] result = new int[currClues.size()];
+        for (int i = 0; i < currClues.size(); i++) {
+            result[i] = currClues.get(i);
+        }
+		return result;
     }
 
     // Sets board's describing row clues based on what's in the board.
     public void updateRowClues() {
     	
     	for (int r = 0; r < rows; r++) {
-            bool[] row = new bool[cols];
-            // Fill in the row by looping through columns.
-            // Call generateClues(row) to get the clues.
-            // Add the clues to rowClues.
-	}
+            boolean[] rowLine = new boolean[cols];
+            for (int c = 0; c < cols; c++) {
+               rowLine[c] = squares[r][c];
+            }
+            rowClues.add(generateClues(rowLine));
+		}
     	
     }
     
-    //Checks if this board's describing clues are the same as a set of given row clues
-    //(If they are the same, this board is a solution to the given game.)
+    // Checks if this board's describing clues are the same as a set of given row clues.
+    // (If they are the same, this board is a solution to the given game.)
     public boolean checkRowsSolution(ArrayList<int[]> newRowClues) {
-    	if (newRowClues.size() != rowClues.size()) {return false;}
-    	for (int r = 0; r < rowClues.size(); r++) {
-    	   if (rowClues.get(r).length != newRowClues.get(r).length) {return false;}
-    	   if (rowClues.get(r).length != 0) {
-    	   	for (int c = 0; c < rowClues.get(r).length; c++) {
-    	   	   
-    	   	}
-    	   }
-    	   //otherwise they both have length 0 and no comparison happens
+    	if (newRowClues.size() != rowClues.size()) {
+    		return false;
     	}
-    	/*
-    	if arraylists are different sizes: return false
-    	for each array in arraylists:
-    		it's possible that they're both empty arrays; move on in that case
-    		if lengths are different: return false
-    		otherwise check to see if values are same
-    			if they aren't, return false; otherwise, keep going
-
-    			
-    	if it gets all the way to the end of the method then return true
-    	*/
+		for (int r = 0; r < rowClues.size(); r++) {
+			if (rowClues.get(r).length != newRowClues.get(r).length) {
+				return false;
+			}
+			if (rowClues.get(r).length != 0) {
+				for (int c = 0; c < rowClues.get(r).length; c++) {
+					if (rowClues.get(r)[c] != newRowClues.get(r)[c]) {
+						return false;
+					}
+				}
+			}
+    	}
     	return true;
     }
     
-    //For troubleshooting: print out rowClues
+    //For troubleshooting: print out rowClues.
     public void printRowClues() {
         assert rowClues.size() == rows;
         System.err.println();
-	for (int r = 0; r < rows; r++) {
+        for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
                 if (squares[r][c]) {
                     System.err.print('*');
@@ -130,8 +128,8 @@ public class Board {
                 System.err.print(rowClues.get(r)[c]);
             }
             System.err.println();
-	}
+		}
         System.err.println();
-    }
+	}
 }
 
