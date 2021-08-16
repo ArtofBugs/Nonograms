30baecf7087916f10516e54b6e14ff27e6983a55
ArtofBugs <TheArtofBugs@gmail.com>
2021-08-16 11:16:01 -0700

    Added runner.sh

---
+++ runner.sh
@@ -0,0 +1,8 @@
+# Enter the name of the java program as the first argument
+# and the path of the .non file as the second argument.
+javac Backtracking.java
+javac BruteForce.java
+javac Board.java
+javac NonParser.java
+javac Timer.java
+java $1 $2
