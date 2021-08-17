f31488e07d19658497b9286d1d0d0d244c89a38b
ArtofBugs <TheArtofBugs@gmail.com>
2021-08-17 11:35:31 -0700

    Added RCBacktracking.java compile

--- runner.sh
+++ runner.sh
@@ -1,5 +1,6 @@
 # Enter the name of the java program to run (without ".java") as the first
 # argument and the path of the .non file as the second argument.
+javac RCBacktracking.java
 javac Backtracking.java
 javac BruteForce.java
 javac Board.java
