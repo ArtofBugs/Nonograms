12212d3ac00c61502cbf751330f70d2f9ddd1032
Bart Massey <bart.massey@gmail.com>
2021-08-18 14:54:39 -0700

    repurposed runner.sh to be timing script

--- runner.sh
+++ runner.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Enter the name of the java program to run (without ".java") as the first
-# argument and the path of the .non file as the second argument.
-javac RCBacktracking.java
-javac Backtracking.java
-javac BruteForce.java
-javac Board.java
-javac NonParser.java
-javac Timer.java
-java $1 $2
+java BruteForce "$@"
+java Backtracking "$@"
+java RCBacktracking "$@"
